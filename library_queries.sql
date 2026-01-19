73ea4cb20d075c4c44c48d8714b8fad60513e2e5
Roshan-Kujur <Roshankujurcareer5@gmail.com>
2026-01-19 20:54:55 +0530

    Add SQL query to retrieve book borrowing details

--- library_queries.sql
+++ library_queries.sql
@@ -1,1 +1,12 @@
+SELECT 
+    m.member_name,
+    b.title AS book_title,
+    a.author_name,
+    br.issue_date,
+    br.return_date
+FROM book_borrow_records br
+JOIN members m ON br.member_id = m.id
+JOIN books b ON br.book_id = b.book_id
+LEFT JOIN authors a ON b.author_id = a.author_id;
+
 
