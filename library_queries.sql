8238ecd01d1f06a5f60f59494316a5a7057551fc
Roshan-Kujur <Roshankujurcareer5@gmail.com>
2026-01-20 11:42:07 +0530

    Update library_queries.sql

--- library_queries.sql
+++ library_queries.sql
@@ -1,3 +1,4 @@
+-- So this query will retrieve the data of previous borrow records but given the books have been returned
 SELECT 
     m.member_name,
     b.title AS book_title,
@@ -9,7 +10,7 @@ JOIN members m ON br.member_id = m.id
 JOIN books b ON br.book_id = b.book_id
 LEFT JOIN authors a ON b.author_id = a.author_id;
 
--- currently borrowed 
+-- This query will give the data of currently borrowed books
 SELECT 
     m.member_name,
     b.title,
@@ -19,12 +20,12 @@ JOIN members m ON br.member_id = m.id
 JOIN books b ON br.book_id = b.book_id
 WHERE br.return_date IS NULL;
 
--- members who have borrowed more than three books
+-- This query will reveal those members who have borrowed atleast three books or more
 SELECT 
     m.member_name,
     COUNT(br.record_seq) AS total_borrowed
 FROM members m
 JOIN book_borrow_records br ON m.id = br.member_id
 GROUP BY m.member_name
-HAVING COUNT(br.record_seq) > 3;
+HAVING COUNT(br.record_seq) >= 3;
 
